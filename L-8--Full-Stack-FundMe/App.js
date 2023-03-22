59ebdb69900495762af4cf9fc7cca82dcaadc2fc
root <root@Gunjan-PC.localdomain>
2023-03-22 12:12:14 +0530

    Completed HTML/JS FundMe Project

--- L-8--Full-Stack-FundMe/App.js
+++ L-8--Full-Stack-FundMe/App.js
@@ -3,11 +3,15 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
 
+const balanceBtn = document.getElementById("balanceBtn")
 const connectButton = document.getElementById("connectBtn")
 const fundButton = document.getElementById("fundBtn")
+const withdrawBtn = document.getElementById("withdrawBtn")
 connectButton.onclick = connect
 // connectButton.onclick = connect() => this will automatically call connect function without clicking
 fundButton.onclick = fund
+balanceBtn.onclick = getBalance
+withdrawBtn.onclick = withdraw
 // async function connsct() {} , this dont needs to be initialized before call
 // const connect = async () => {}  , this needs to initialized before called
 async function connect() {
@@ -25,8 +29,17 @@ async function connect() {
         connectButton.innerText = "Please get Metamask"
     }
 }
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+        // console.log(balance.toString()) //653600000000000000000
+        // console.log(balance) // _hex :"0x236e85a9142b500000"
+    }
+}
 async function fund() {
-    const ethAmount = "56"
+    const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}...`)
     if (typeof window.ethereum !== "undefined") {
         //to send a transaction we always need
@@ -44,16 +57,72 @@ async function fund() {
 
         const signer = provider.getSigner()
         const contract = new ethers.Contract(contractAddress, abi, signer)
+
         try {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount),
             })
+            console.log("funded " + ethAmount)
+            // wait for transaction to finish
+            await listenForTransactionMined(transactionResponse, provider)
+            /**
+             * funded 56
+             *  Done!
+             * Mining 0x25f8ba5ada49075ba750457cca25b2293d4407861b5e72cd0a57e0eaa6b477c9...
+             * here the order is not correct "Done!" should be last
+             * this is called event loop
+             * provider.once takes time to our machine executes remaining code, our front end
+             * checks for provider.once has completed or not and then it comes again to execute
+             * the code
+             * thst's why we are using Promises
+             * so we want to adjust the code to finish listening to our "listner"(provider.once())
+             * the concept of async/await does not work
+             */
+            console.log("Done!")
         } catch (error) {
             console.log("Rejected!!")
         }
     }
 }
-// window.onload = connect();
+// only owner will be able to withdraw (first acc in hardhat node)
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Withdrawing...");
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withDraw()
+            await listenForTransactionMined(transactionResponse,provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+function listenForTransactionMined(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    // reason for promise is bcz  we need to create listner for blockchain
+    // provider.once(eventName,listner)
+    // provider.once  transactionResponse.hash happens and execute the function
+    // the promies is gonna be done(reslove) when the listner finishes listening
+
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} Block confirmations`
+            )
+            resolve() // transactionResponse.hash fires we are going to reslove this promise
+            // inside of provider.once so transactionResponse.hash is found it resolves
+            // means once the transactionResponse.hash found the we are going to call this function
+            // (transactionRectiprt) =>{...}
+            // Promise only returns when reject or reslove is called
+            // and we are telling it to reslove once provider.once completes
+        })
+    })
+}
+
+// window.onload = connect()
 
 // in node.js => require
-/// in frontend => import  , require wont work , better way to do
+// in frontend => import  , require wont work , better way to do
+// anonymus function notation "() => {}"
