e1449132e9f63844f49756b663f5fd033ffaf12a
GunjanSurti <85667477+GunjanSurti@users.noreply.github.com>
2023-03-23 11:53:30 +0530

    dependencies added

--- L-9 HH-SmartContract-Lottery-Raffle/hardhat.config.js
+++ L-9 HH-SmartContract-Lottery-Raffle/hardhat.config.js
@@ -1,3 +1,10 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
